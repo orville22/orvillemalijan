401682004f60875125b277cfd40bc4cbaf994bcd
Orville Malijan <orville@creativiumdigital.com>
2023-08-07 15:38:28 +1000

    minor fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,10 @@
 export default function Contact() {
   return (
     <div className="section contact-page">
-      <h1>Contact Me</h1>
+      <h1>Get in touch</h1>
       <p>Feel free to give me a call or an email!</p>
       <h2>
-        <a href="tel:123-456-7890">☎️ 0420691848</a>
+        <a href="tel:0420691848">☎️ 0420691848</a>
       </h2>
       <h2>
         <a href="mailto:ormalijan@gmail.com">📧 ormalijan@gmail.com</a>
